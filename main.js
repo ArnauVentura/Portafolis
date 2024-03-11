751b4e05426010e5fd83d19c32c1413570b27b46
Arnau <aventurac2122@politecnics.barcelona>
2024-03-11 17:13:03 +0100

    added image box

---
+++ main.js
@@ -0,0 +1,21 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const slider = document.querySelector('.slider');
+//     const slides = document.querySelectorAll('.slider img');
+//     const slideWidth = slides[0].clientWidth;
+//     let counter = 1;
+
+//     setInterval(() => {
+//         slider.style.transition = 'transform 0.5s ease';
+//         slider.style.transform = `translateX(${-slideWidth * counter}px)`;
+
+//         counter++;
+
+//         if (counter === slides.length) {
+//             setTimeout(() => {
+//                 slider.style.transition = 'none';
+//                 slider.style.transform = `translateX(0)`;
+//             }, 500);
+//             counter = 1;
+//         }
+//     }, 3000); // Change slide every 3 seconds
+// });
