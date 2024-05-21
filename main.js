c5234dfb9f095035a57bd6970ff41f362296ec92
Arnau Ventura <arnau.ventura2002@gmail.com>
2024-05-21 19:05:56 +0200

    popup

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const slider = document.querySelector('.slider');
-//     const slides = document.querySelectorAll('.slider img');
-//     const slideWidth = slides[0].clientWidth;
-//     let counter = 1;
-
-//     setInterval(() => {
-//         slider.style.transition = 'transform 0.5s ease';
-//         slider.style.transform = `translateX(${-slideWidth * counter}px) rotate(0deg)`; // Adjust rotation to 0 degrees
-
-//         counter++;
-
-//         if (counter === slides.length) {
-//             setTimeout(() => {
-//                 slider.style.transition = 'none';
-//                 slider.style.transform = `translateX(0) rotate(0deg)`; // Adjust rotation to 0 degrees
-//             }, 500);
-//             counter = 1;
-//         }
-//     }, 3000); // Change slide every 3 seconds
-// });
+document.addEventListener('DOMContentLoaded', (event) => {
+    var popup = document.getElementById('popup');
+    var btn = document.getElementById('projectsBtn');
+    var closeBtn = document.getElementById('closePopup');
+  
+    btn.onclick = function() {
+      popup.style.display = 'block';
+    }
+  
+    closeBtn.onclick = function() {
+      popup.style.display = 'none';
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = 'none';
+      }
+    }
+  });
+  
