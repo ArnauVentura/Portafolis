ca7c609d5bec3ba4bc497b5a3d494ac78eef19a0
Arnau Ventura <arnau.ventura2002@gmail.com>
2024-05-22 21:05:25 +0200

    popup vsc

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   });
   
+document.getElementById('lenguajes').addEventListener('change', function() {
+    const selectedLanguage = this.value;
+    const codeImage = document.getElementById('codeImage');
+
+    switch (selectedLanguage) {
+        case 'php':
+            codeImage.src = 'images/php.png';
+            break;
+        case 'html':
+            codeImage.src = 'images/html.png';
+            break;
+        case 'css':
+            codeImage.src = 'images/css.png';
+            break;
+        case 'js':
+            codeImage.src = 'images/js.png';
+            break;
+        default:
+            codeImage.src = 'images/php.png';
+    }
+});
