b20d0d279f8ea70b69e0bd4c31bed932c7c6463c
Arnau <aventurac2122@politecnics.barcelona>
2024-05-27 16:42:25 +0200

    added images

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    var popup = document.getElementById('popup');
-    var btn = document.getElementById('projectsBtn');
-    var closeBtn = document.getElementById('closePopup');
-  
-    btn.onclick = function() {
-      popup.style.display = 'block';
-    }
-  
-    closeBtn.onclick = function() {
+  var popup = document.getElementById('popup');
+  var btn = document.getElementById('projectsBtn');
+  var closeBtn = document.getElementById('closePopup');
+
+  btn.onclick = function() {
+    popup.style.display = 'block';
+  }
+
+  closeBtn.onclick = function() {
+    popup.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target == popup) {
       popup.style.display = 'none';
     }
-  
-    window.onclick = function(event) {
-      if (event.target == popup) {
-        popup.style.display = 'none';
-      }
-    }
-  });
-  
-document.getElementById('lenguajes').addEventListener('change', function() {
-    const selectedLanguage = this.value;
-    const codeImage = document.getElementById('codeImage');
+  }
 
-    switch (selectedLanguage) {
-        case 'php':
-            codeImage.src = 'images/php.png';
-            break;
-        case 'html':
-            codeImage.src = 'images/html.png';
-            break;
-        case 'css':
-            codeImage.src = 'images/css.png';
-            break;
-        case 'js':
-            codeImage.src = 'images/js.png';
-            break;
-        default:
-            codeImage.src = 'images/php.png';
-    }
+  var lenguajes = document.getElementById('lenguajes');
+  var codeImage = document.getElementById('codeImage');
+
+  var images = {
+    php: 'Source/php.png',
+    html: 'Source/html.png',
+    css: 'Source/css.png',
+    js: 'Source/js.png'
+  };
+
+  lenguajes.addEventListener('change', function() {
+    var selectedValue = this.value;
+    codeImage.src = images[selectedValue];
+  });
 });
+
